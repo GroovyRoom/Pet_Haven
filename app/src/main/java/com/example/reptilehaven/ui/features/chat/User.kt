d82592254c582bcfa9fadd1e41a01d4cb9ffd9ea
Jason Lee <l.chang.suk@gmail.com>
2022-07-31 15:50:28 -0700

    Edited User model to add phoneNumber and String

--- app/src/main/java/com/example/reptilehaven/ui/features/chat/User.kt
+++ app/src/main/java/com/example/reptilehaven/ui/features/chat/User.kt
@@ -1,5 +1,5 @@
 package com.example.reptilehaven.ui.features.chat
 
-class User(val uid: String, val username: String, val profileImageUrl: String) {
-    constructor() : this("", "", "")
+class User(val uid: String, val username: String, val profileImageUrl: String, val phoneNumber: String, val address: String) {
+    constructor() : this("", "", "", "", "")
 }
