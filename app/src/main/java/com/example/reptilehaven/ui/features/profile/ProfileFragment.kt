9e4ab322c7dd2d7d973e25ff5960abb258ccc5b4
LesterLi33 <leelisooke@gmail.com>
2022-07-31 17:39:45 -0700

    userproflie UI

--- app/src/main/java/com/example/reptilehaven/ui/features/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/reptilehaven/ui/features/profile/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.example.reptilehaven.ui.features.profile
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.reptilehaven.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
@@ -14,7 +18,22 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var user = FirebaseAuth.getInstance().currentUser
+        tv_name.setText(user?.displayName?:"")
+        tv_email.setText(user?.email?:"")
+        if(user?.photoUrl!=null){
+            iv_head.setImageURI(user?.photoUrl)
+        }
+        val profileUpdates = userProfileChangeRequest {
+            displayName = "Jane Q. User"
+            photoUri = Uri.parse("https://example.com/jane-q-user/profile.jpg")
+        }
+        tv_phone.setText(user?.phoneNumber?:"")
     }
 }
