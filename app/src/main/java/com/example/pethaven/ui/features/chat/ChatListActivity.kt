1ad89d0cde3c909a3800e687353feaf074e2f5f5
Jason Lee <l.chang.suk@gmail.com>
2022-08-01 22:27:58 -0700

    Implementing ChatListActivity.

--- app/src/main/java/com/example/pethaven/ui/features/chat/ChatListActivity.kt
+++ app/src/main/java/com/example/pethaven/ui/features/chat/ChatListActivity.kt
@@ -6,28 +6,124 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.pethaven.R
 import com.example.pethaven.ui.LoginActivity
+import com.example.pethaven.ui.features.chat.NewChatActivity.Companion.USER_KEY
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_chat_list.*
+import kotlinx.android.synthetic.main.activity_chat_log.*
+import kotlinx.android.synthetic.main.latest_message_row.view.*
 
 class ChatListActivity  : AppCompatActivity() {
 
     companion object {
         var currentUser: User? = null
+        val TAG = "ChatListActivity"
     }
 
+    val adapter = GroupAdapter<ViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_list)
 
+        recyclerview_latest_messages.adapter = adapter
+        recyclerview_latest_messages.addItemDecoration(DividerItemDecoration(this,
+            DividerItemDecoration.VERTICAL))
+
+        adapter.setOnItemClickListener {item, view ->
+            Log.d(TAG, "Testing Click on Message")
+            val intent = Intent(this, ChatLogActivity::class.java)
+
+            val row = item as LatestMessageRow
+            row.chatPartnerUser
+            intent.putExtra(NewChatActivity.USER_KEY, row.chatPartnerUser)
+            startActivity(intent)
+        }
+
+        listenForLatestMessages()
+
         fetchCurrentUser()
 
         checkUserLoggedIn()
+    }
+
+    val latestMessagesMap = HashMap<String, ChatMessage>()
+
+    private fun refreshRecyclerViewMessages() {
+        adapter.clear()
+        latestMessagesMap.values.forEach {
+            adapter.add(LatestMessageRow(it))
+        }
+    }
+
+    private fun listenForLatestMessages() {
+        val fromId = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance().getReference("/latest-messages/$fromId")
+        ref.addChildEventListener(object: ChildEventListener {
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                val chatMessage = p0.getValue(ChatMessage::class.java) ?: return
+                latestMessagesMap[p0.key!!] = chatMessage
+                refreshRecyclerViewMessages()
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, previousChildName: String?) {
+                val chatMessage = p0.getValue(ChatMessage::class.java) ?: return
+                latestMessagesMap[p0.key!!] = chatMessage
+                refreshRecyclerViewMessages()
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, previousChildName: String?) {
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+        })
+    }
+
+    class LatestMessageRow(val chatMessage: ChatMessage): Item<ViewHolder>() {
+        var chatPartnerUser: User? = null
+
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+            viewHolder.itemView.message_textview_latest_message.text = chatMessage.text
+
+            val chatPartnerId: String
+            if (chatMessage.fromId == FirebaseAuth.getInstance().uid) {
+                chatPartnerId = chatMessage.toId
+            } else {
+                chatPartnerId = chatMessage.fromId
+            }
+            val ref = FirebaseDatabase.getInstance().getReference("/users/$chatPartnerId")
+            ref.addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(p0: DataSnapshot) {
+                    val chatPartnerUser = p0.getValue(User::class.java)
+                    viewHolder.itemView.username_textview_latest_message.text = chatPartnerUser?.username
+
+                    val targetImageView = viewHolder.itemView.imageview_latest_message
+                    Picasso.get().load(chatPartnerUser?.profileImageUrl).into(targetImageView)
+                }
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+            })
+
+            viewHolder.itemView.username_textview_latest_message.text = "theName"
+        }
+
+        override fun getLayout(): Int {
+            return R.layout.latest_message_row
+        }
+
     }
 
     private fun fetchCurrentUser() {
