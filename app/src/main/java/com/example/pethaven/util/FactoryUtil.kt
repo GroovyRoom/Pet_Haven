a43b2feb97c73f93aa3732f7d23ede9d197f9261
Jason Lee <l.chang.suk@gmail.com>
2022-08-07 12:24:07 -0700

    j

--- app/src/main/java/com/example/pethaven/util/FactoryUtil.kt
+++ app/src/main/java/com/example/pethaven/util/FactoryUtil.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.example.pethaven.domain.ReptileDao
 import com.example.pethaven.domain.ReptileRepository
 import com.example.pethaven.domain.ReptileViewModelFactory
+import com.example.pethaven.domain.ReptileViewModelFactoryFav
 
 object FactoryUtil {
     fun generateReptileViewModelFactory(context: Context) : ReptileViewModelFactory {
